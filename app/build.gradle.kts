7afa3da7b312e7adf79b92a550b1f4b9d3a60cd4
Himanshu Mehra <onlineorderapp2020@gmail.com>
2026-07-02 18:14:04 +0530

    added firebase crashlytics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.spiritual.brihaspativarkatha"
         minSdk = 24
         targetSdk = 36
-        versionCode = 13
-        versionName = "2.1"
+        versionCode = 14
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
