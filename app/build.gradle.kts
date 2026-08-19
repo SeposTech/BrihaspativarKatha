e0dae6ac8a8b018102c9dbc11eb26cd4d06135e1
Himanshu Mehra <onlineorderapp2020@gmail.com>
2026-08-19 20:13:54 +0530

    update version for mantra jaap

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.spiritual.brihaspativarkatha"
         minSdk = 24
         targetSdk = 36
-        versionCode = 15
-        versionName = "2.3"
+        versionCode = 16
+        versionName = "2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
